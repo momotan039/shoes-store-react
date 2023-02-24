b03670a5363d3a1f6f48ad9412dc5ec18b9bb4f0
momotan <momotaha039@gmail.com>
2023-02-24 20:53:09 +0200

    add root file to make a structure

---
+++ src/components/Root.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect, useMemo } from "react";
+import { Outlet} from "react-router-dom";
+import { DeleteShoe, getShoes } from "../utils/crud.mjs";
+import { rootRefs } from "../utils/local.mjs";
+import Header from "./header/Header";
+import GridSpinner from "./spinner/GridSpinner.jsx";
+
+export default function Root() {
+  const [shoes, setShoe] = useState();
+
+  rootRefs.reCallData=async()=>{
+    const shs= await getShoes()
+    setShoe(shs)
+    history.pushState(shs,'stam')
+    return shs
+  }
+
+  useEffect(() => {
+    rootRefs.reCallData()
+  }, []);
+
+  return (
+    <>
+      {
+      shoes ? 
+         <>
+         <Header/>
+         <Outlet />
+       </>
+      : 
+        <GridSpinner/>
+      }
+    </>
+    
+  );
+}
